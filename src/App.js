3ccfbb77800b7cad2e5d41de75fc25756abf4908
Pablo Dominguez <pasaxo@gmail.com>
2022-10-26 19:31:40 -0300

    Requisito 4

--- src/App.js
+++ src/App.js
@@ -3,13 +3,60 @@ import Form from './components/Form';
 import Card from './components/Card';
 
 class App extends React.Component {
+  state = {
+    name: '',
+    description: '',
+    attr1: '',
+    attr2: '',
+    attr3: '',
+    image: '',
+    rare: '',
+    trunfo: false,
+  };
+
+  onInputChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
+    const { name,
+      description,
+      attr1,
+      attr2,
+      attr3,
+      image,
+      rare,
+      trunfo,
+    } = this.state;
+
     return (
       <div>
         <h1>ADICIONE NOVA CARTA</h1>
-        <Form />
+        <Form
+          cardName={ name }
+          cardDescription={ description }
+          cardAttr1={ attr1 }
+          cardAttr2={ attr2 }
+          cardAttr3={ attr3 }
+          cardImage={ image }
+          cardRare={ rare }
+          cardTrunfo={ trunfo }
+          onInputChange={ this.onInputChange }
+        />
         <h1>PRÉ-VISUALIZAÇÃO</h1>
-        <Card />
+        <Card
+          cardName={ name }
+          cardDescription={ description }
+          cardAttr1={ attr1 }
+          cardAttr2={ attr2 }
+          cardAttr3={ attr3 }
+          cardImage={ image }
+          cardRare={ rare }
+          cardTrunfo={ trunfo }
+        />
       </div>
     );
   }
